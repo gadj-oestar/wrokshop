b5505b7c8da98d8605ca7b30fe3fd02345e26e35
gadj-oestar <122262636+gadj-oestar@users.noreply.github.com>
2023-06-04 23:48:02 +0200

    work-shop

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 AOS.init({
-  offset: 180, // offset (in px) from the original trigger point
+  offset: 200, // offset (in px) from the original trigger point
   delay: 170, // values from 0 to 3000, with step 50ms
   duration: 1000,
 });
